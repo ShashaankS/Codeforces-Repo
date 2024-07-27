cee2c0adf4266892966237503a4ce413428d9f6b
Shashaank Srivastava <140889387+ShashaankS@users.noreply.github.com>
2024-07-27 23:45:28 +0530

    Updates

---
+++ 1857A.cpp
@@ -0,0 +1,51 @@
+using lli=long long int;
+using ll= long long;
+using ull=unsigned long long;
+#define fori for(ll i=0; i<n; i++)
+#define forj for(ll j=0; j<n; j++)
+#define form for(ll i=0; i<m; i++)
+#define fors for(ll i=0; i<s.length(); i++)
+#define forv for(ll i=0; i<v.size(); i++)
+#define forst for(auto i=st.begin(); i!=st.end(); i++)
+#define formst for(auto i=mst.begin(); i!=mst.end(); i++)
+#define mp make_pair
+#define pb push_back
+#define eb emplace_back
+#define f first
+#define s second
+#define sorta sort(a,a+n)
+#define sortv sort(v.begin(), v.end())
+#define sorts sort(s.begin(), s.end())
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int n;
+        cin>>n;
+        int a[n];
+        fori{cin>>a[i];}
+
+        sort(a,a+n,greater<int>());
+        lli sum=0;
+        fori{sum+=a[i];}
+        lli preSum=0;
+        int flag=0;
+        for(int i=0;i<n;i++)
+        {
+            preSum+=a[i];
+            if(preSum%2==0 && (sum-preSum)%2==0)
+            {flag=1;break;}
+            
+        }
+        if(flag==1)
+        cout<<"YES"<<endl;
+        else
+        cout<<"NO"<<endl;
+    }
+    return 0;
+}
